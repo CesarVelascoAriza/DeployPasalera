03ba18a022a5532a7e865136c102cb681034ab8e
Adolfo <Adolfo@192.168.230.1>
2020-11-06 21:28:47 -0500

    jajja

---
+++ src/main/java/co/edu/ucentral/PsapasarelaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsapasarelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsapasarelaApplication.class, args);
+	}
+
+}
